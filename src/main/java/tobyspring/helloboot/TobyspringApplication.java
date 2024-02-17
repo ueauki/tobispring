3071b510483b7863c15ba9e5658dc2b2cbfd00a3
ryo <ryo@delivered.co.kr>
2024-02-17 22:18:42 +0900

    feature: test 코드 작성중

--- src/main/java/tobyspring/helloboot/TobyspringApplication.java
+++ src/main/java/tobyspring/helloboot/TobyspringApplication.java
@@ -7,12 +7,16 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -23,68 +27,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@SpringBootApplication
+//@Spring
+// BootApplication
+@Configuration
+@ComponentScan
 public class TobyspringApplication {
-
-    public HelloController helloController() {
-        return new HelloController();
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
-    public SimpleHelloService helloService() {
-        return new SimpleHelloService();
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 
     public static void main(String[] args) {
 //        SpringApplication.run(TobyspringApplication.class, args);
+        MySpringApplication.run(TobyspringApplication.class, args);
 
-        // spring container 생성
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
-
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                // servelet container(tomcat) 생성
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-                    servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(this)
-                    ).addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
     }
 }
-
-
-
-//            servletContext.addServlet("hello", new HttpServlet() {
-//                @Override
-//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-//                        String name = req.getParameter("name");
-//
-//                        // 응답값 바인딩
-//                        resp.setStatus(HttpStatus.OK.value());
-//                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-//
-//                        // 등록했던 bean class 를 사용할 수 있게 get
-//                        HelloController helloController = applicationContext.getBean(HelloController.class);
-//
-//                        String ret = helloController.hello(name);
-//                        resp.getWriter().println(ret);
-//
-//                    }
-//                    else {
-//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-//                    }
-//                }
-//            }
-//            ).addMapping("/*");
---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,29 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?param={param}", String.class, "spring");
+
+        // status 200
+        Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header text/plain
+        Assertions.assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+
+        // body hello spring
+        Assertions.assertThat(res.getBody()).isEqualTo("hello spring");
+
+    }
+}
